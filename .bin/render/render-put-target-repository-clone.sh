15a37c570a35369591feb0f3bc79725ed4720ca0
Esten Rye <esten.rye@ryezone.com>
2026-04-23 23:36:47 -0500

    add check for var

--- .bin/render/render-put-target-repository-clone.sh
+++ .bin/render/render-put-target-repository-clone.sh
@@ -4,13 +4,15 @@ SCRIPTS_DIR=${SCRIPTS_DIR:-$(cd "$(dirname "$0")/.." && pwd)}
 RENDER_DIR=${RENDER_DIR:-$(cd "$(dirname "$SCRIPTS_DIR")/.render" && pwd)}
 TARGET_REPO_OWNER=${TARGET_REPO_OWNER:-$(${SCRIPTS_DIR}/render/render-get-source-repository-owner.sh)}
 TARGET_REPO_NAME=${TARGET_REPO_NAME:-flux-platform-rendered}
-GH_TOKEN=${RENDER_GITHUB_TOKEN:-$(gh auth token)}
 
 echo "Cloning Target Repository: ${TARGET_REPO_OWNER}/${TARGET_REPO_NAME}"
 if [ -z "$GH_TOKEN" ]; then
   echo "Error: GH_TOKEN is not set. Please set the RENDER_GITHUB_TOKEN environment variable or ensure you are authenticated with gh."
   exit 1
 fi
+
+GH_TOKEN=${RENDER_GITHUB_TOKEN:-$(gh auth token)}
+
 pushd ${RENDER_DIR} > /dev/null || exit 1
 rm -rf ${TARGET_REPO_NAME}
 gh repo clone ${TARGET_REPO_OWNER}/${TARGET_REPO_NAME} -- --depth 1
