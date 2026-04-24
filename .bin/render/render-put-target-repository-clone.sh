3a7c89b75ed80748f38d62de5acee09628af158c
Esten Rye <esten.rye@ryezone.com>
2026-04-23 23:58:50 -0500

    fix ordering of error messages

--- .bin/render/render-put-target-repository-clone.sh
+++ .bin/render/render-put-target-repository-clone.sh
@@ -4,6 +4,7 @@ SCRIPTS_DIR=${SCRIPTS_DIR:-$(cd "$(dirname "$0")/.." && pwd)}
 RENDER_DIR=${RENDER_DIR:-$(cd "$(dirname "$SCRIPTS_DIR")/.render" && pwd)}
 TARGET_REPO_OWNER=${TARGET_REPO_OWNER:-$(${SCRIPTS_DIR}/render/render-get-source-repository-owner.sh)}
 TARGET_REPO_NAME=${TARGET_REPO_NAME:-flux-platform-rendered}
+GH_TOKEN=${RENDER_GITHUB_TOKEN:-$(gh auth token)}
 
 echo "Cloning Target Repository: ${TARGET_REPO_OWNER}/${TARGET_REPO_NAME}"
 if [ -z "$GH_TOKEN" ]; then
@@ -11,7 +12,6 @@ if [ -z "$GH_TOKEN" ]; then
   exit 1
 fi
 
-GH_TOKEN=${RENDER_GITHUB_TOKEN:-$(gh auth token)}
 
 pushd ${RENDER_DIR} > /dev/null || exit 1
 rm -rf ${TARGET_REPO_NAME}
